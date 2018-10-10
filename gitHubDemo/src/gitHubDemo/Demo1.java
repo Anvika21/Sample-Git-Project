aa12a92cf0b31a3e24283212eb31d2ee4add7d11
Satish.Banda <Satish.Banda@P016443.iascorp.co.uk>
2018-10-10 10:44:43 +0100

    This is the initial project commit to loacal Rpository
    Change-Id: Ic9cb10a9ac593a2f13c4dacbbdd43c7f77e0b524

---
+++ gitHubDemo/src/gitHubDemo/Demo1.java
@@ -0,0 +1,11 @@
+package gitHubDemo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my git hub demo project");
+
+	}
+
+}
